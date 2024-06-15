cdf3560850dfd0de7a34cfb3cd25ada2cbea94b0
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2024-06-15 13:49:34 +0300

    authservice refactor

--- authservice/src/test/java/edu/sabanciuniv/authservice/AuthserviceApplicationTests.java
+++ authservice/src/test/java/edu/sabanciuniv/authservice/AuthserviceApplicationTests.java
@@ -1,13 +1,13 @@
-package edu.sabanciuniv.auth;
+package edu.sabanciuniv.authservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuthApplicationTests {
+class AuthserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
