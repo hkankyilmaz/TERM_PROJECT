b99d5c76eb724619301ea7a6bcd9ba519532bc77
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2024-07-24 00:45:41 +0300

    created -> default layout

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,11 +1,42 @@
-<template>
-  <div>
-    <p>Header</p>
-    <slot />
-    <p>Footer</p>
-  </div>
-</template>
-
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<template lang="pug">
+
+.main-container
+  navbar
+    span.logo
+      nuxt-link(to="/") Home
+    ul
+      li
+        NuxtLink(to="/about") About
+      li
+        NuxtLink(to="/contact") Contact
+      li
+        NuxtLink(to="/blog") Blog
+  main
+    slot
+  footer Footer
+
+</template>
+
+<style scoped lang="scss">
+@use "@/styles/index.scss" as *;
+
+.main-container {
+  min-height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+
+navbar {
+  width: 100vw;
+  background-color: $main-color;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
